068486ba5073ac3dbdbae0142eac653bf6d1d401
Jason Balthis <jason.balthis@gmail.com>
2014-12-02 16:43:34 -0500

    initial commit
    WordPress plugin to gather metrics on various aspects of use of Cisco's
    internal ENG-TME WebApp.

---
+++ doc/index.php
@@ -0,0 +1,2 @@
+// block direct access to plugin PHP files
+defined('ABSPATH') or die('No script kitties please, meooooow ;P');
---
+++ engtme-metrics.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name: ENG-TME Metrics
+ * Plugin URI: http://github.com/jbalthis/engtme-metrics/doc/
+ * Description: WordPress plugin to gather metrics on various aspects of use of Cisco's internal ENG-TME WebApp.
+ * Version: 0.1
+ * Author: Jason Balthis
+ * Author URI: http://github.com/jbalthis
+ * License: GPL2
+ */
+
+// block direct access to plugin PHP files
+defined('ABSPATH') or die('No script kitties please, meooooow ;P');
+
+
